469753d105dca83dcdf49e10f3a37923c0567176
eazybytes <tutor@eazybytes.com>
2026-05-18 18:58:44 +0530

    Teaching LLMs to Remember - The Power of Chat Memory in Spring AI

--- section04/springai/src/main/java/com/eazybytes/springai/controller/PromptStuffingController.java
+++ section04/springai/src/main/java/com/eazybytes/springai/controller/PromptStuffingController.java
@@ -1,9 +1,8 @@
 package com.eazybytes.springai.controller;
 
+import com.openai.models.ChatModel;
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.openai.OpenAiChatOptions;
-import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,8 +27,7 @@ public class PromptStuffingController {
     public String promptStuffing(@RequestParam("message") String message) {
         return chatClient
                 .prompt()
-                .options(OpenAiChatOptions.builder()
-                        .model(OpenAiApi.ChatModel.CHATGPT_4_O_LATEST).build())
+                .options(OpenAiChatOptions.builder().model(ChatModel.GPT_5_4_MINI.asString())/*.maxCompletionTokens(20)*/)
                 .system(systemPromptTemplate)
                 .user(message)
                 .call().content();
