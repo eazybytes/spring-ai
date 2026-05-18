3a0b6ccae8eee796516d0b548893b22ba53f7020
eazybytes <tutor@eazybytes.com>
2026-05-18 21:03:14 +0530

    Transcription, Speech, and Image Generation using Spring AI

--- section_10/springai/src/main/java/com/eazybytes/springai/controller/AudioController.java
+++ section_10/springai/src/main/java/com/eazybytes/springai/controller/AudioController.java
@@ -1,13 +1,16 @@
 package com.eazybytes.springai.controller;
 
+import com.openai.models.audio.AudioResponseFormat;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
+import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
+import org.springframework.ai.audio.transcription.TranscriptionModel;
+import org.springframework.ai.audio.tts.TextToSpeechModel;
+import org.springframework.ai.audio.tts.TextToSpeechOptions;
+import org.springframework.ai.audio.tts.TextToSpeechPrompt;
+import org.springframework.ai.audio.tts.TextToSpeechResponse;
 import org.springframework.ai.openai.OpenAiAudioSpeechOptions;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
-import org.springframework.ai.openai.api.OpenAiAudioApi;
-import org.springframework.ai.openai.audio.speech.SpeechModel;
-import org.springframework.ai.openai.audio.speech.SpeechPrompt;
-import org.springframework.ai.openai.audio.speech.SpeechResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,34 +27,36 @@ import java.nio.file.Paths;
 @RequestMapping("/api")
 public class AudioController {
 
-    private final OpenAiAudioTranscriptionModel openAiAudioTranscriptionModel;
-    private final SpeechModel speechModel;
+    private final TranscriptionModel transcriptionModel;
+    private final TextToSpeechModel textToSpeechModel;
 
-    AudioController(OpenAiAudioTranscriptionModel transcriptionModel,
-            SpeechModel speechModel) {
-        this.openAiAudioTranscriptionModel = transcriptionModel;
-        this.speechModel = speechModel;
+    AudioController(TranscriptionModel transcriptionModel, TextToSpeechModel textToSpeechModel) {
+        this.transcriptionModel = transcriptionModel;
+        this.textToSpeechModel = textToSpeechModel;
     }
 
     @GetMapping("/transcribe")
     String transcribe(@Value("classpath:SpringAI.mp3") Resource audioFile) {
-        return openAiAudioTranscriptionModel.call(audioFile);
+        AudioTranscriptionPrompt prompt = new AudioTranscriptionPrompt(
+                audioFile);
+        AudioTranscriptionResponse response = transcriptionModel.call(prompt);
+        return response.getResult().getOutput();
     }
 
     @GetMapping("/transcribe-options")
     String transcribeWithOptions(@Value("classpath:SpringAI.mp3") Resource audioFile) {
-        var audioTranscriptionResponse = openAiAudioTranscriptionModel.call(new AudioTranscriptionPrompt(
+        var audioTranscriptionResponse = transcriptionModel.call(new AudioTranscriptionPrompt(
                 audioFile, OpenAiAudioTranscriptionOptions.builder()
                 .prompt("Talking about Spring AI")
                 .language("en")
                 .temperature(0.5f)
-                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.VTT).build()));
+                .responseFormat(AudioResponseFormat.VTT).build()));
         return audioTranscriptionResponse.getResult().getOutput();
     }
 
     @GetMapping("/speech")
-    String spech(@RequestParam("message") String message) throws IOException {
-        byte[] audioBytes = speechModel.call(message);
+    String speech(@RequestParam("message") String message) throws IOException {
+        byte[] audioBytes = textToSpeechModel.call(message);
         Path path = Paths.get("output.mp3");
         Files.write(path, audioBytes);
         return "MP3 saved successfully to " + path.toAbsolutePath();
@@ -59,16 +64,14 @@ public class AudioController {
 
     @GetMapping("/speech-options")
     String spechWithOptions(@RequestParam("message") String message) throws IOException {
-        SpeechResponse speechResponse = speechModel.call(new SpeechPrompt(message,
-                OpenAiAudioSpeechOptions.builder().voice(OpenAiAudioApi.SpeechRequest.Voice.NOVA)
-                        .speed(2.0f)
-                        .responseFormat(OpenAiAudioApi.SpeechRequest.AudioResponseFormat.MP3).build()));
+        TextToSpeechResponse speechResponse = textToSpeechModel.call(new TextToSpeechPrompt(message,
+                TextToSpeechOptions.builder().voice(OpenAiAudioSpeechOptions.Voice.NOVA.getValue())
+                        .speed(2.0)
+                        .format(OpenAiAudioSpeechOptions.AudioResponseFormat.MP3.getValue()).build()));
         Path path = Paths.get("speech-options.mp3");
         Files.write(path, speechResponse.getResult().getOutput());
         return "MP3 saved successfully to " + path.toAbsolutePath();
     }
-
-
 
 
 }
--- section_10/springai/src/main/java/com/eazybytes/springai/controller/ImageController.java
+++ section_10/springai/src/main/java/com/eazybytes/springai/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.eazybytes.springai.controller;
 
 import org.springframework.ai.image.ImageModel;
 import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageModel;
 import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,28 +13,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ImageController {
 
-    private final ImageModel imageModel;
+    private final OpenAiImageModel openAiImageModel;
 
-    public ImageController(ImageModel imageModel) {
-        this.imageModel = imageModel;
+    public ImageController(OpenAiImageModel openAiImageModel) {
+        this.openAiImageModel = openAiImageModel;
     }
 
     @GetMapping("/image")
     String genateImage(@RequestParam("message") String message) {
-        var imageResponse = imageModel.call(new ImagePrompt(message));
-        return imageResponse.getResult().getOutput().getUrl();
+        var imageResponse = openAiImageModel.call(new ImagePrompt(message,
+                OpenAiImageOptions.builder()
+                        .model("gpt-image-2").build()));
+        return imageResponse.getResults().get(0).getOutput().getB64Json();
     }
 
     @GetMapping("/image-options")
     String genateImageWithOptions(@RequestParam("message") String message) {
-        var imageResponse = imageModel.call(new ImagePrompt(message,
+        var imageResponse = openAiImageModel.call(new ImagePrompt(message,
                 OpenAiImageOptions.builder()
                         .N(1)
-                        .quality("hd")
-                        .style("natural")
-                        .height(1024)
-                        .width(1024).responseFormat("url").build()));
-        return imageResponse.getResult().getOutput().getUrl();
+                        .model("gpt-image-2").build()));
+        return imageResponse.getResults().get(0).getOutput().getB64Json();
     }
 
 }
